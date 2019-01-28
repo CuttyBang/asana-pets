3453d5db9a304e57047ee7e88f69d8f73b1f3703
Nathan McElwain <nathanmcelwain@ymail.com>
2019-01-27 23:35:56 -0500

    fixing webpack config

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import "./main.scss";
 import Vue from 'vue'
 import App from './App.vue'
+import VueImg from 'v-img';
 
+Vue.use(VueImg);
 
 new Vue({
   el: '#app',
--- webpack.config.js
+++ webpack.config.js
@@ -69,7 +69,7 @@ module.exports = {
           {
             loader: "babel-loader",
             options: {
-              presets: ['env', 'es2015']
+              presets: ['env']
             }
           }
         ]
